10840bb23e5dfade021a627b64a60dc435d74a33
Fatih Enes Selvi <benzer1010@gmail.com>
2023-01-11 15:12:00 +0300

    Base Component added

---
+++ src/app/base/base.component.ts
@@ -0,0 +1,21 @@
+import { NgxSpinnerService } from "ngx-spinner";
+
+export class BaseComponent {
+  constructor(private spinner: NgxSpinnerService) { }
+
+  showSpinner(spinnerType: SpinnerType) {
+    this.spinner.show(spinnerType);
+    setTimeout(() => this.hideSpinner(spinnerType), 500)
+  }
+
+  hideSpinner(spinnerType: SpinnerType) {
+    this.spinner.hide(spinnerType);
+    setTimeout(() => this.hideSpinner(spinnerType), 500)
+  }
+}
+
+export enum SpinnerType {
+  BallAtom = "s1",
+  BallScaleMultiple = "s2",
+  BallSpinClokwiseFadeRotating = "s3",
+}
