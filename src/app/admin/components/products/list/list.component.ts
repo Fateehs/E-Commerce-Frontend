f639cdc6c16e6f4fef804dfdbedcc5a78b44deec
Fatih Enes Selvi <benzer1010@gmail.com>
2023-02-03 18:32:08 +0300

    Pagination configured

--- src/app/admin/components/products/list/list.component.ts
+++ src/app/admin/components/products/list/list.component.ts
@@ -1,37 +1,65 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource, _MatTableDataSource } from '@angular/material/table';
+import {
+  MatTableDataSource,
+  _MatTableDataSource,
+} from '@angular/material/table';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { List_Product } from 'src/app/contracts/list_product';
-import { AlertifyMessageType, AlertifyService, Position } from 'src/app/services/admin/alertify.service';
+import {
+  AlertifyMessageType,
+  AlertifyService,
+  Position,
+} from 'src/app/services/admin/alertify.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent extends BaseComponent implements OnInit {
-
-  displayedColumns: string[] = ['name', 'stock', 'price', 'createdDate', 'updatedDate'];
+  displayedColumns: string[] = [
+    'name',
+    'stock',
+    'price',
+    'createdDate',
+    'updatedDate',
+  ];
   dataSource: MatTableDataSource<List_Product> = null;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor(spinner: NgxSpinnerService, private productService: ProductService, private alertifyService: AlertifyService) {
-    super(spinner)
+  constructor(
+    spinner: NgxSpinnerService,
+    private productService: ProductService,
+    private alertifyService: AlertifyService
+  ) {
+    super(spinner);
   }
 
   async getProducts() {
     this.showSpinner(SpinnerType.BallAtom);
-    const allProducts: List_Product[] = await this.productService.read(() => this.hideSpinner(SpinnerType.BallAtom),
-      errorMessage => this.alertifyService.message(errorMessage, {
-        dismissOthers: true,
-        messageType: AlertifyMessageType.Error,
-        position: Position.TopRight
-      }));
-    this.dataSource = new MatTableDataSource<List_Product>(allProducts);
-    this.dataSource.paginator = this.paginator;
+    const allProducts: { totalCount: number; products: List_Product[] } =
+      await this.productService.read(
+        this.paginator ? this.paginator.pageIndex : 0,
+        this.paginator ? this.paginator.pageSize : 5,
+        () => this.hideSpinner(SpinnerType.BallAtom),
+        (errorMessage) =>
+          this.alertifyService.message(errorMessage, {
+            dismissOthers: true,
+            messageType: AlertifyMessageType.Error,
+            position: Position.TopRight,
+          })
+      );
+    this.dataSource = new MatTableDataSource<List_Product>(
+      allProducts.products
+    );
+    this.paginator.length = allProducts.totalCount;
+  }
+
+  async pageChanged() {
+    await this.getProducts();
   }
 
   async ngOnInit() {
