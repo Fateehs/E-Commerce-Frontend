0aa81bb65427cb324eac0313032a6ab6e1cbb3ad
Fatih Enes Selvi <benzer1010@gmail.com>
2023-02-03 16:34:58 +0300

    Product List Table configured

--- src/app/admin/components/products/list/list.component.ts
+++ src/app/admin/components/products/list/list.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource, _MatTableDataSource } from '@angular/material/table';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { List_Product } from 'src/app/contracts/list_product';
+import { AlertifyMessageType, AlertifyService, Position } from 'src/app/services/admin/alertify.service';
+import { ProductService } from 'src/app/services/common/models/product.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent {
+export class ListComponent extends BaseComponent implements OnInit {
 
+  displayedColumns: string[] = ['name', 'stock', 'price', 'createdDate', 'updatedDate'];
+  dataSource: MatTableDataSource<List_Product> = null;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(spinner: NgxSpinnerService, private productService: ProductService, private alertifyService: AlertifyService) {
+    super(spinner)
+  }
+
+  async getProducts() {
+    this.showSpinner(SpinnerType.BallAtom);
+    const allProducts: List_Product[] = await this.productService.read(() => this.hideSpinner(SpinnerType.BallAtom),
+      errorMessage => this.alertifyService.message(errorMessage, {
+        dismissOthers: true,
+        messageType: AlertifyMessageType.Error,
+        position: Position.TopRight
+      }));
+    this.dataSource = new MatTableDataSource<List_Product>(allProducts);
+    this.dataSource.paginator = this.paginator;
+  }
+
+  async ngOnInit() {
+    await this.getProducts();
+  }
 }
