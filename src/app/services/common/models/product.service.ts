7483a2bec289310f12ec81c14840579cf0a5dc94
Fatih Enes Selvi <benzer1010@gmail.com>
2023-02-03 16:35:36 +0300

    Select Operation added

--- src/app/services/common/models/product.service.ts
+++ src/app/services/common/models/product.service.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Create_Product } from 'src/app/contracts/create_product';
+import { List_Product } from 'src/app/contracts/list_product';
 import { HttpClientService } from '../http-client.service';
 
 @Injectable({
@@ -9,7 +10,7 @@ import { HttpClientService } from '../http-client.service';
 export class ProductService {
   constructor(private httpClientService: HttpClientService) { }
 
-  create(product: Create_Product, successCallBack?: any, errorCallBack?: any) {
+  create(product: Create_Product, successCallBack?: any, errorCallBack?: (errorMessage: string) => void) {
     this.httpClientService
       .post(
         {
@@ -29,5 +30,16 @@ export class ProductService {
         });
         errorCallBack(message);
       });
+  }
+
+  async read(page: number = 0, size: number = 5, successCallBack?: () => void, errorCallBack?: (erroMessage: string) => void): Promise<List_Product[]> {
+    const promiseData: Promise<List_Product[]> = this.httpClientService.get<List_Product[]>({
+      fullEndPoint: "https://localhost:7235/api/products"
+    }).toPromise();
+
+    promiseData.then(d => successCallBack())
+      .catch((errorResponse: HttpErrorResponse) => errorCallBack(errorResponse.message))
+
+    return await promiseData;
   }
 }
