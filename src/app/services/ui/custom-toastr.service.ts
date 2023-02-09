19dba7f5f58ba10b3c26ca5c6fc70d6955c4609a
Fatih Enes Selvi <benzer1010@gmail.com>
2023-02-09 17:40:27 +0300

    Service configured

--- src/app/services/ui/custom-toastr.service.ts
+++ src/app/services/ui/custom-toastr.service.ts
@@ -2,38 +2,40 @@ import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CustomToastrService {
-
-  constructor(private toastr: ToastrService) { }
-  message(message: string, title: string, toastrOptions: Partial<ToastrOptions>) {
+  constructor(private toastr: ToastrService) {}
+  message(
+    message: string,
+    title: string,
+    toastrOptions: Partial<ToastrOptions>
+  ) {
     this.toastr[toastrOptions.messageType](message, title, {
-      positionClass: toastrOptions.position
-    })
+      positionClass: toastrOptions.position,
+    });
   }
 }
 
 export class ToastrOptions {
   messageType: ToastrMessageType;
-  position: ToastrPosition
+  position: ToastrPosition;
 }
 
 export enum ToastrMessageType {
-  Success = "success",
-  Info = "info",
-  Warning = "warning",
-  Error = "error"
+  Success = 'success',
+  Info = 'info',
+  Warning = 'warning',
+  Error = 'error',
 }
 
 export enum ToastrPosition {
-  TopRight = "toast-top-right",
-  BottomRight = "toast-bottom-right",
-  BottomLeft = "toast-bottom-left",
-  TopLeft = "toast-top-left",
-  TopFullWidth = "toast-top-full-width",
-  BottomFullWidth = "toast-bottom-full-width",
-  TopCenter = "toast-top-center",
-  BottomCenter = "toast-bottom-center"
+  TopRight = 'toast-top-right',
+  BottomRight = 'toast-bottom-right',
+  BottomLeft = 'toast-bottom-left',
+  TopLeft = 'toast-top-left',
+  TopFullWidth = 'toast-top-full-width',
+  BottomFullWidth = 'toast-bottom-full-width',
+  TopCenter = 'toast-top-center',
+  BottomCenter = 'toast-bottom-center',
 }
-
