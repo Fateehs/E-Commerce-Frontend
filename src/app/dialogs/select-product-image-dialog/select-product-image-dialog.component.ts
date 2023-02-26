0176710a9f14ae4c8e796e6a96c635df75c9c4e9
Fateeh <benzer1010@gmail.com>
2023-02-26 18:38:03 +0300

    Select Product Image added

---
+++ src/app/dialogs/select-product-image-dialog/select-product-image-dialog.component.ts
@@ -0,0 +1,31 @@
+import { Component, Inject, OnInit, Output } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FileUploadOptions } from 'src/app/services/common/file-upload/file-upload.component';
+import { BaseDialog } from '../base/base-dialog';
+
+@Component({
+  selector: 'app-select-product-image-dialog',
+  templateUrl: './select-product-image-dialog.component.html',
+  styleUrls: ['./select-product-image-dialog.component.scss']
+})
+export class SelectProductImageDialogComponent extends BaseDialog<SelectProductImageDialogComponent> {
+
+  constructor(dialogRef: MatDialogRef<SelectProductImageDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: SelectProductImageState | string) {
+    super(dialogRef);
+
+  }
+
+  @Output() options: Partial<FileUploadOptions> = {
+    accept: ".png, .jpg, .jpeg, .gif ",
+    action: "upload",
+    controller: "products",
+    explanation : "You can select or drag product images.",
+    isAdminPage: true,
+    queryString: `id=${this.data}`
+  }
+}
+
+export enum SelectProductImageState {
+  Close
+}
