adab2a75931a8fcd7480c7357d748987b5d8b593
Fateeh <benzer1010@gmail.com>
2023-02-28 17:19:35 +0300

    Spinner and JS added

--- src/app/dialogs/select-product-image-dialog/select-product-image-dialog.component.ts
+++ src/app/dialogs/select-product-image-dialog/select-product-image-dialog.component.ts
@@ -1,28 +1,50 @@
 import { Component, Inject, OnInit, Output } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NgxSpinner, NgxSpinnerService } from 'ngx-spinner';
+import { SpinnerType } from 'src/app/base/base.component';
+import { List_Product_Image } from 'src/app/contracts/list_product_image';
 import { FileUploadOptions } from 'src/app/services/common/file-upload/file-upload.component';
+import { ProductService } from 'src/app/services/common/models/product.service';
 import { BaseDialog } from '../base/base-dialog';
+
+declare var $: any
 
 @Component({
   selector: 'app-select-product-image-dialog',
   templateUrl: './select-product-image-dialog.component.html',
   styleUrls: ['./select-product-image-dialog.component.scss']
 })
-export class SelectProductImageDialogComponent extends BaseDialog<SelectProductImageDialogComponent> {
+export class SelectProductImageDialogComponent extends BaseDialog<SelectProductImageDialogComponent> implements OnInit {
 
   constructor(dialogRef: MatDialogRef<SelectProductImageDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: SelectProductImageState | string) {
+    @Inject(MAT_DIALOG_DATA) public data: SelectProductImageState | string,
+    private productService: ProductService,
+    private spinner: NgxSpinnerService) {
     super(dialogRef);
-
   }
 
   @Output() options: Partial<FileUploadOptions> = {
     accept: ".png, .jpg, .jpeg, .gif ",
     action: "upload",
     controller: "products",
-    explanation : "You can select or drag product images.",
+    explanation: "You can select or drag product images.",
     isAdminPage: true,
     queryString: `id=${this.data}`
+  }
+
+  images: List_Product_Image[];
+
+  async ngOnInit() {
+    this.spinner.show(SpinnerType.BallAtom)
+    this.images = await this.productService.readImages(this.data as string, () => this.spinner.hide(SpinnerType.BallAtom))
+  }
+
+  async deleteImage(imageId: string) {
+    this.spinner.show(SpinnerType.BallAtom)
+    await this.productService.deleteImage(this.data as string, imageId, () => {
+      this.spinner.hide(SpinnerType.BallAtom);
+      $(this).fadeOut(500);
+    });
   }
 }
 
