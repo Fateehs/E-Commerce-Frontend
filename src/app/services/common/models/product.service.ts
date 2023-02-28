f28a81753f4a607c54d494260912c4497c2fb520
Fateeh <benzer1010@gmail.com>
2023-02-28 17:19:53 +0300

    Methods added

--- src/app/services/common/models/product.service.ts
+++ src/app/services/common/models/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { firstValueFrom, Observable } from 'rxjs';
 import { Create_Product } from 'src/app/contracts/create_product';
 import { List_Product } from 'src/app/contracts/list_product';
+import { List_Product_Image } from 'src/app/contracts/list_product_image';
 import { HttpClientService } from '../http-client.service';
 
 @Injectable({
@@ -19,7 +20,7 @@ export class ProductService {
     this.httpClientService
       .post(
         {
-          controller : "products"
+          controller: "products"
         },
         product
       )
@@ -72,5 +73,25 @@ export class ProductService {
     }, id);
 
     await firstValueFrom(deleteObservable);
+  }
+
+  async readImages(id: string, successCallBack?: () => void): Promise<List_Product_Image[]> {
+    const getObservable: Observable<List_Product_Image[]> = this.httpClientService.get<List_Product_Image[]>({
+      action: "getimages",
+      controller: "products"
+    }, id);
+    const images: List_Product_Image[] = await firstValueFrom(getObservable);
+    successCallBack();
+    return images;
+  }
+
+  async deleteImage(id: string, imageId: string, successCallBack?: () => void) {
+    const deleteObservable = this.httpClientService.delete({
+      action: "deleteImage",
+      controller: "products",
+      queryString: `imageId=${imageId}`
+    }, id)
+    await firstValueFrom(deleteObservable);
+    successCallBack();
   }
 }
