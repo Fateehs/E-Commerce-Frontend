bcede7661f24f5086ab6bd4ba100c1e149f41cb4
Fateeh <benzer1010@gmail.com>
2023-03-02 22:54:25 +0300

    Register Module imported

--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from 'src/app/admin/components/orders/orders.module';
-import { CustomersComponent } from 'src/app/admin/components/customers/customers.component';
+import { RegisterModule } from './register/register.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  ],
   imports: [
     CommonModule,
     ProductsModule,
     OrdersModule,
+    RegisterModule
   ]
 })
 export class ComponentsModule { }
