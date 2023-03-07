5f1ef2ccd60975cf3ac31737e1a3e8c4984907a5
Fateeh <benzer1010@gmail.com>
2023-03-07 19:36:42 +0300

    NGX Spinner, Toastr Service added

--- src/app/ui/components/register/register.component.ts
+++ src/app/ui/components/register/register.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { NgxSpinner, NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent } from 'src/app/base/base.component';
+import { Create_User } from 'src/app/contracts/users/create_user';
 import { User } from 'src/app/entities/user';
+import { UserService } from 'src/app/services/common/models/user.service';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from 'src/app/services/ui/custom-toastr.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent extends BaseComponent implements OnInit {
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private toastrService: CustomToastrService,
+    spinner: NgxSpinnerService) {
+    super(spinner)
+  }
 
   registerFormGroup: FormGroup;
   ngOnInit(): void {
@@ -23,7 +34,7 @@ export class RegisterComponent implements OnInit {
       validators: (group: AbstractControl): ValidationErrors | null => {
         let password = group.get('password').value;
         let confirmPassword = group.get('confirmPassword').value
-        return password === confirmPassword ? null : {notSame: true}
+        return password === confirmPassword ? null : { notSame: true }
       }
     })
   }
@@ -33,11 +44,23 @@ export class RegisterComponent implements OnInit {
   }
 
   submitted: boolean = false;
-  onSubmit(user: User) {
+  async onSubmit(user: User) {
     this.submitted = true;
     // var c = this.component;
     // debugger;
     if (this.registerFormGroup.invalid)
       return;
+
+    const result: Create_User = await this.userService.create(user);
+    if (result.succeeded)
+      this.toastrService.message(result.message, "User registration successful.", {
+        messageType: ToastrMessageType.Success,
+        position: ToastrPosition.TopRight
+      })
+    else
+      this.toastrService.message(result.message, "User registration not successful.", {
+        messageType: ToastrMessageType.Error,
+        position: ToastrPosition.TopRight
+      })
   }
 }
