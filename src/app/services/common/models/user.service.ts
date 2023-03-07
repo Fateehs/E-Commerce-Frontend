69733631abebe47656ef33be2ef71c0a742dda58
Fateeh <benzer1010@gmail.com>
2023-03-07 19:35:13 +0300

    User Login added, Methods added

---
+++ src/app/services/common/models/user.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { firstValueFrom, Observable } from 'rxjs';
+import { Token } from 'src/app/contracts/token/token';
+import { TokenResponse } from 'src/app/contracts/token/tokenResponse';
+import { Create_User } from 'src/app/contracts/users/create_user';
+import { User } from 'src/app/entities/user';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../ui/custom-toastr.service';
+import { HttpClientService } from '../http-client.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private httpClientService: HttpClientService,
+    private toastrService: CustomToastrService) { }
+
+  async create(user: User): Promise<Create_User> {
+    const observable: Observable<Create_User | User> = this.httpClientService.post<Create_User | User>({
+      controller: "users"
+    }, user);
+
+    return await firstValueFrom(observable) as Create_User;
+  }
+
+  async login(usernameOrEmail: string, password: string, callBackFunction?: () => void): Promise<any> {
+    const observable: Observable<any | TokenResponse> = this.httpClientService.post<any | TokenResponse>({
+      controller: "users",
+      action: "login"
+    }, { usernameOrEmail, password })
+
+    const tokenResponse: TokenResponse = await firstValueFrom(observable) as TokenResponse;
+    if (tokenResponse) {
+      localStorage.setItem("accessToken", tokenResponse.token.accessToken);
+
+      this.toastrService.message("User login successful", "Login Successful", {
+        messageType: ToastrMessageType.Success,
+        position: ToastrPosition.TopRight
+      })
+    }
+    callBackFunction();
+  }
+}
---
+++ src/app/ui/components/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { UserService } from 'src/app/services/common/models/user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent extends BaseComponent implements OnInit {
+
+  constructor(
+    private userService: UserService,
+    spinner: NgxSpinnerService) {
+    super(spinner)
+  }
+
+  ngOnInit(): void {
+  }
+
+  async login(usernameOrEmail: string, password: string) {
+    this.showSpinner(SpinnerType.BallAtom);
+    await this.userService.login(usernameOrEmail, password, () => this.hideSpinner(SpinnerType.BallAtom));
+  }
+}
