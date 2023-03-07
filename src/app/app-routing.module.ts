5f4226396277d481c6e5850aba281eb8bf30618a
Fateeh <benzer1010@gmail.com>
2023-03-07 19:33:12 +0300

    Guard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './admin/components/dashboard/dashboard.component';
 import { LayoutComponent } from './admin/layout/layout.component';
+import { AuthGuard } from './guards/common/auth.guard';
 import { HomeComponent } from './ui/components/home/home.component';
 
 const routes: Routes = [
@@ -20,12 +21,13 @@ const routes: Routes = [
         path: "orders", loadChildren: () => import("./admin/components/orders/orders.module").then
           (module => module.OrdersModule)
       }
-    ]
+    ], canActivate:[AuthGuard]
   },
   { path: "", component: HomeComponent },
   { path: "basket", loadChildren: () => import("./ui/components/baskets/baskets.module").then(module => module.BasketsModule) },
   { path: "products", loadChildren: () => import("./ui/components/products/products.module").then(module => module.ProductsModule) },
   { path: "register", loadChildren: () => import("./ui/components/register/register.module").then(module => module.RegisterModule) },
+  { path: "login", loadChildren: () => import("./ui/components/login/login.module").then(module => module.LoginModule) },
 ];
 
 @NgModule({
