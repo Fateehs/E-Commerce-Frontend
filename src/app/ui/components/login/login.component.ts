7d6f7a249a727bbb57da020b6a8e7c1b6e3ac9e2
Fateeh <benzer1010@gmail.com>
2023-03-09 19:28:41 +0300

    Return Url Navigation added

--- src/app/ui/components/login/login.component.ts
+++ src/app/ui/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { AuthService } from 'src/app/services/common/auth.service';
 import { UserService } from 'src/app/services/common/models/user.service';
 
 @Component({
@@ -12,7 +14,10 @@ export class LoginComponent extends BaseComponent implements OnInit {
 
   constructor(
     private userService: UserService,
-    spinner: NgxSpinnerService) {
+    spinner: NgxSpinnerService,
+    private authService: AuthService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router) {
     super(spinner)
   }
 
@@ -21,6 +26,16 @@ export class LoginComponent extends BaseComponent implements OnInit {
 
   async login(usernameOrEmail: string, password: string) {
     this.showSpinner(SpinnerType.BallAtom);
-    await this.userService.login(usernameOrEmail, password, () => this.hideSpinner(SpinnerType.BallAtom));
+    await this.userService.login(usernameOrEmail, password, () => {
+      this.authService.identityCheck();
+
+      this.activatedRoute.queryParams.subscribe(params => {
+        const returnUrl: string = params["returnUrl"];
+        if (returnUrl)
+          this.router.navigate([returnUrl])
+      })
+
+      this.hideSpinner(SpinnerType.BallAtom)
+    });
   }
 }
