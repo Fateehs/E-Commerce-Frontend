a357df1674ca83f7acfedc28dbce4afdeabf56bf
Fateeh <benzer1010@gmail.com>
2023-03-09 19:26:47 +0300

    Guard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,17 +11,17 @@ const routes: Routes = [
       { path: "", component: DashboardComponent },
       {
         path: "customers", loadChildren: () => import("./admin/components/customers/customers.module").then
-          (module => module.CustomersModule)
+          (module => module.CustomersModule), canActivate: [AuthGuard]
       },
       {
         path: "products", loadChildren: () => import("./admin/components/products/products.module").then
-          (module => module.ProductsModule)
+          (module => module.ProductsModule), canActivate: [AuthGuard]
       },
       {
         path: "orders", loadChildren: () => import("./admin/components/orders/orders.module").then
-          (module => module.OrdersModule)
+          (module => module.OrdersModule), canActivate: [AuthGuard]
       }
-    ], canActivate:[AuthGuard]
+    ], canActivate: [AuthGuard]
   },
   { path: "", component: HomeComponent },
   { path: "basket", loadChildren: () => import("./ui/components/baskets/baskets.module").then(module => module.BasketsModule) },
