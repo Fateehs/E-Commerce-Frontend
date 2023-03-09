66840d8b43a87c99d778ca8535361076ca9d7c67
Fateeh <benzer1010@gmail.com>
2023-03-09 19:27:22 +0300

    Sign out added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { AuthService } from './services/common/auth.service';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 declare var $: any
 
 @Component({
@@ -8,4 +11,20 @@ declare var $: any
 })
 export class AppComponent {
   title = 'ETradeApplication';
+  constructor(public authService: AuthService,
+    private toastrService: CustomToastrService,
+    private router: Router) {
+    authService.identityCheck();
+  }
+
+  signOut() {
+    localStorage.removeItem("accessToken");
+    this.authService.identityCheck();
+    this.router.navigate([""])
+    if (this.authService.isAuthenticated == false)
+      this.toastrService.message("Logout successful", "Signed out", {
+        messageType: ToastrMessageType.Error,
+        position: ToastrPosition.TopRight
+      });
+  }
 }
