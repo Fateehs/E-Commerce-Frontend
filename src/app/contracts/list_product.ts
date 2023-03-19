a1aaa7cff006dbb049ca09012edb8bb655504236
Fateeh <benzer1010@gmail.com>
2023-03-19 17:13:28 +0300

    Product Contracts added

--- src/app/contracts/list_product.ts
+++ src/app/contracts/list_product.ts
@@ -1,8 +1,12 @@
+import { List_Product_Image } from "./list_product_image";
+
 export class List_Product {
     id: string;
     name: string;
     stock: number;
     price: number;
-    createDate: Date;
+    createdDate: Date;
     updatedDate: Date;
+    productImageFiles: List_Product_Image[];
+    imagePath : string;
 }
