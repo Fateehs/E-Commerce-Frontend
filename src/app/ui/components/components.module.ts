131581b9c05bac8f068ebbc8dfe4567f92c679a1
Fateeh <benzer1010@gmail.com>
2023-03-23 15:48:35 +0300

    Module exported

--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -4,6 +4,7 @@ import { ProductsModule } from './products/products.module';
 import { OrdersModule } from 'src/app/admin/components/orders/orders.module';
 import { RegisterModule } from './register/register.module';
 import { LoginModule } from './login/login.module';
+import { BasketsModule } from './baskets/baskets.module';
 
 
 
@@ -16,6 +17,9 @@ import { LoginModule } from './login/login.module';
     OrdersModule,
     RegisterModule,
     // LoginModule
+  ],
+  exports:[
+    BasketsModule
   ]
 })
 export class ComponentsModule { }
