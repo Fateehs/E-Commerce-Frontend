a73b872587729e14d0a52978d1b2078903307d88
Fateeh <benzer1010@gmail.com>
2023-03-23 15:49:51 +0300

    Component modified, Method added

--- src/app/ui/components/products/list/list.component.ts
+++ src/app/ui/components/products/list/list.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { BaseUrl } from 'src/app/contracts/base_url';
+import { Create_Basket_Item } from 'src/app/contracts/basket/create_basket_item';
 import { List_Product } from 'src/app/contracts/list_product';
+import { BasketService } from 'src/app/services/common/models/basket.service';
 import { FileService } from 'src/app/services/common/models/file.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from 'src/app/services/ui/custom-toastr.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit {
+export class ListComponent extends BaseComponent implements OnInit {
 
   constructor(private productService: ProductService,
     private activatedRoute: ActivatedRoute,
-    private fileService: FileService) { }
+    private fileService: FileService,
+    private basketService: BasketService,
+    private customToastrService: CustomToastrService,
+    spinner: NgxSpinnerService) {
+    super(spinner)
+  }
 
   currentPageNo: number;
   totalProductCount: number;
@@ -53,7 +63,7 @@ export class ListComponent implements OnInit {
           updatedDate: p.updatedDate,
           productImageFiles: p.productImageFiles
         };
-        
+
         return listProduct;
       });
 
@@ -74,7 +84,23 @@ export class ListComponent implements OnInit {
         for (let i = this.currentPageNo - 3; i <= this.currentPageNo + 3; i++)
           this.pageList.push(i);
     });
+  }
 
+  async addToBasket(product: List_Product) {
+    this.showSpinner(SpinnerType.BallAtom);
+
+    let _basketItem: Create_Basket_Item = new Create_Basket_Item();
+    _basketItem.productId = product.id;
+    _basketItem.quantity = 1;
+
+    await this.basketService.add(_basketItem);
+
+    this.hideSpinner(SpinnerType.BallAtom);
+    this.customToastrService.message("Product added to basket", "Product Added",
+      {
+        position: ToastrPosition.TopRight,
+        messageType: ToastrMessageType.Success
+      })
   }
 
 }
