d34d3523d8497a3267ba1d008b650cbbf45311af
Fateeh <benzer1010@gmail.com>
2023-03-25 18:10:47 +0300

    Method added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Route, Router } from '@angular/router';
+import { DynamicLoadComponentDirective } from './directives/common/dynamic-load-component.directive';
 import { AuthService } from './services/common/auth.service';
+import { ComponentType, DynamicLoadComponentService } from './services/common/dynamic-load-component.service';
 import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 declare var $: any
 
@@ -10,10 +12,14 @@ declare var $: any
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild(DynamicLoadComponentDirective, { static: true })
+  dynamicLoadComponentDirective: DynamicLoadComponentDirective;
+
   title = 'ETradeApplication';
   constructor(public authService: AuthService,
     private toastrService: CustomToastrService,
-    private router: Router) {
+    private router: Router,
+    private dynamicLoadComponentService: DynamicLoadComponentService) {
     authService.identityCheck();
   }
 
@@ -26,5 +32,9 @@ export class AppComponent {
         messageType: ToastrMessageType.Error,
         position: ToastrPosition.TopRight
       });
+  }
+
+  loadComponent() {
+    this.dynamicLoadComponentService.loadComponent(ComponentType.BasketsComponent, this.dynamicLoadComponentDirective.viewContainerRef);
   }
 }
