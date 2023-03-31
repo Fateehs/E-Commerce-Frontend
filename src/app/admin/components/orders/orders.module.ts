4942a458b3914a801813915d3b6b1dc13344c438
Fateeh <benzer1010@gmail.com>
2023-03-31 15:22:26 +0300

    Modules, Component imported

--- src/app/admin/components/orders/orders.module.ts
+++ src/app/admin/components/orders/orders.module.ts
@@ -2,18 +2,36 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrdersComponent } from './orders.component';
 import { RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { DialogModule } from 'src/app/dialogs/dialog/dialog.module';
+import { FileUploadModule } from 'src/app/services/common/file-upload/file-upload.module';
 
 
 
 @NgModule({
   declarations: [
-    OrdersComponent
+    OrdersComponent,
+    ListComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([
-      {path: "", component:OrdersComponent}
-    ])
+      { path: "", component: OrdersComponent }
+    ]),
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    DialogModule,
+    FileUploadModule
   ]
 })
 export class OrdersModule { }
