2afddf730023e8a53dca257c1a1e42bb44d4d0d8
Fateeh <benzer1010@gmail.com>
2023-04-06 19:07:26 +0300

    Dialog created, Dependencies added

---
+++ src/app/dialogs/dialog.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { SelectProductImageDialogComponent } from './select-product-image-dialog/select-product-image-dialog.component';
+import { FileUploadModule } from '../services/common/file-upload/file-upload.module';
+import { MatCardModule } from '@angular/material/card';
+import { BasketItemRemoveDialogComponent } from './basket-item-remove-dialog/basket-item-remove-dialog.component';
+import { CompleteShoppingDialogComponent } from './complete-shopping-dialog/complete-shopping-dialog.component';
+import { OrderDetailDialogComponent } from './order-detail-dialog/order-detail-dialog.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [
+    DeleteDialogComponent,
+    SelectProductImageDialogComponent,
+    BasketItemRemoveDialogComponent,
+    CompleteShoppingDialogComponent,
+    OrderDetailDialogComponent
+  ],
+  imports: [
+    CommonModule,
+    MatDialogModule, MatButtonModule, MatCardModule, MatTableModule, MatToolbarModule,
+    FileUploadModule
+  ]
+})
+export class DialogModule { }
