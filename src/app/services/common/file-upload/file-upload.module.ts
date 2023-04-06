d5d81d10b42f550150b51f1cb15f085966e3d57b
Fateeh <benzer1010@gmail.com>
2023-04-06 19:13:37 +0300

    Declarations fixed

--- src/app/services/common/file-upload/file-upload.module.ts
+++ src/app/services/common/file-upload/file-upload.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FileUploadComponent } from './file-upload.component';
 import { NgxFileDropModule } from 'ngx-file-drop';
-import { DialogModule } from 'src/app/dialogs/dialog/dialog.module';
 import { FileUploadDialogComponent } from 'src/app/dialogs/file-upload-dialog/file-upload-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
