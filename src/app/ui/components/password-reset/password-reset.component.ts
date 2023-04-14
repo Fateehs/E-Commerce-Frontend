4d9f7db2246587ce61598bf303a58d9595208990
Fateeh <benzer1010@gmail.com>
2023-04-14 16:26:44 +0300

    Password Reset Feature added

--- src/app/ui/components/password-reset/password-reset.component.ts
+++ src/app/ui/components/password-reset/password-reset.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { AlertifyMessageType, AlertifyPosition, AlertifyService } from 'src/app/services/admin/alertify.service';
+import { UserAuthService } from 'src/app/services/common/models/user-auth.service';
 
 @Component({
   selector: 'app-password-reset',
   templateUrl: './password-reset.component.html',
   styleUrls: ['./password-reset.component.scss']
 })
-export class PasswordResetComponent implements OnInit {
+export class PasswordResetComponent extends BaseComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    spinner: NgxSpinnerService,
+    private userAuthService: UserAuthService,
+    private alertifyService: AlertifyService
+  ) {
+    super(spinner)
   }
 
+  passwordReset(email: string) {
+    this.showSpinner(SpinnerType.BallAtom)
+    this.userAuthService.passwordReset(email, () => {
+      this.hideSpinner(SpinnerType.BallAtom)
+      this.alertifyService.message("Şifre yenileme talebi mail adresinize gönderilmiştir.", {
+        messageType: AlertifyMessageType.Success,
+        position: AlertifyPosition.TopRight
+      })
+    })
+  }
 }
