f605eae3c2d2b7a35fd559338c488290b7aaf84f
Fateeh <benzer1010@gmail.com>
2023-04-22 16:14:17 +0300

    Password Reset Feature added

--- src/app/ui/components/update-password/update-password.component.ts
+++ src/app/ui/components/update-password/update-password.component.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { AlertifyMessageType, AlertifyPosition, AlertifyService } from 'src/app/services/admin/alertify.service';
+import { UserAuthService } from 'src/app/services/common/models/user-auth.service';
+import { UserService } from 'src/app/services/common/models/user.service';
 
 @Component({
   selector: 'app-update-password',
   templateUrl: './update-password.component.html',
   styleUrls: ['./update-password.component.scss']
 })
-export class UpdatePasswordComponent implements OnInit {
+export class UpdatePasswordComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(spinner: NgxSpinnerService, private userAuthService: UserAuthService, private activatedRoute: ActivatedRoute, private alertifyService: AlertifyService, private userService: UserService, private router: Router) {
+    super(spinner)
   }
 
+  state: any;
+
+  ngOnInit(): void {
+    this.showSpinner(SpinnerType.BallAtom)
+    this.activatedRoute.params.subscribe({
+      next: async params => {
+        const userId: string = params["userId"];
+        const resetToken: string = params["resetToken"];
+        this.state = await this.userAuthService.verifyResetToken(resetToken, userId, () => {
+          this.hideSpinner(SpinnerType.BallAtom);
+        })
+      }
+    });
+  }
+
+  updatePassword(password: string, passwordConfirm: string) {
+    this.showSpinner(SpinnerType.BallAtom);
+    if (password != passwordConfirm) {
+      this.alertifyService.message("Şifreleri doğrulayınız!", {
+        messageType: AlertifyMessageType.Error,
+        position: AlertifyPosition.TopRight
+      });
+      this.hideSpinner(SpinnerType.BallAtom)
+      return;
+    }
+
+    this.activatedRoute.params.subscribe({
+      next: async params => {
+        const userId: string = params["userId"];
+        const resetToken: string = params["resetToken"];
+        await this.userService.updatePassword(userId, resetToken, password, passwordConfirm,
+          () => {
+            this.alertifyService.message("Şifre başarıyla güncellenmiştir.", {
+              messageType: AlertifyMessageType.Success,
+              position: AlertifyPosition.TopRight
+            })
+            this.router.navigate(["/login"])
+          },
+          error => {
+            console.log(error)
+          });
+        this.hideSpinner(SpinnerType.BallAtom)
+      }
+    })
+  }
 }
