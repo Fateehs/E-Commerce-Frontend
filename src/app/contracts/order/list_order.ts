83dd8e79696161e84cb71161b573ceff4aa4545b
Fateeh <benzer1010@gmail.com>
2023-04-23 10:01:35 +0300

    Property added

--- src/app/contracts/order/list_order.ts
+++ src/app/contracts/order/list_order.ts
@@ -1,6 +1,7 @@
 export class List_Order {
-    orderCode: string;
-    userName: string;
-    totalPrice: number;
-    createdDate: Date;
-  }
+  orderCode: string;
+  userName: string;
+  totalPrice: number;
+  createdDate: Date;
+  completed: boolean;
+}
