e862ef8a0b9be6ec1f481baa0c3968ee90514d9b
Fateeh <benzer1010@gmail.com>
2023-04-28 12:02:48 +0300

    Authorization Feature added

---
+++ src/app/admin/components/authorize-menu/authorize-menu.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthorizeMenuComponent } from './authorize-menu.component';
+import { RouterModule } from '@angular/router';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
+
+@NgModule({
+  declarations: [
+    AuthorizeMenuComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      { path: "", component: AuthorizeMenuComponent }
+    ]),
+    MatTreeModule, MatIconModule, MatButtonModule
+  ]
+})
+export class AuthorizeMenuModule { }
