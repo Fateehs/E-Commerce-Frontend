9b4f72f0bf089e318ed29c176077382dbc9666f4
Fateeh <benzer1010@gmail.com>
2023-04-28 12:02:09 +0300

    Route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,6 +20,10 @@ const routes: Routes = [
       {
         path: "orders", loadChildren: () => import("./admin/components/orders/orders.module").then
           (module => module.OrdersModule), canActivate: [AuthGuard]
+      },
+      {
+        path: "authorize-menu", loadChildren: () => import("./admin/components/authorize-menu/authorize-menu.module").then
+          (module => module.AuthorizeMenuModule), canActivate: [AuthGuard]
       }
     ], canActivate: [AuthGuard]
   },
