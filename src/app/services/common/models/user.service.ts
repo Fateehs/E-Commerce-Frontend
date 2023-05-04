6a97ec5210e5604d26b724bf61bfd31b6377988e
Fateeh <benzer1010@gmail.com>
2023-05-04 09:09:59 +0300

    Service modified

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { firstValueFrom, Observable } from 'rxjs';
-import { Create_User } from 'src/app/contracts/users/create_user';
-import { User } from 'src/app/entities/user';
+import { Create_User } from '../../../contracts/users/create_user';
+import { List_User } from '../../../contracts/users/list_user';
+import { User } from '../../../entities/user';
 import { CustomToastrService } from '../../ui/custom-toastr.service';
 import { HttpClientService } from '../http-client.service';
 
@@ -9,9 +10,7 @@ import { HttpClientService } from '../http-client.service';
   providedIn: 'root'
 })
 export class UserService {
-
-  constructor(private httpClientService: HttpClientService,
-    private toastrService: CustomToastrService) { }
+  constructor(private httpClientService: HttpClientService, private toastrService: CustomToastrService) { }
 
   async create(user: User): Promise<Create_User> {
     const observable: Observable<Create_User | User> = this.httpClientService.post<Create_User | User>({
@@ -35,5 +34,47 @@ export class UserService {
     const promiseData: Promise<any> = firstValueFrom(observable);
     promiseData.then(value => successCallBack()).catch(error => errorCallBack(error));
     await promiseData;
+  }
+
+  async getAllUsers(page: number = 0, size: number = 5, successCallBack?: () => void, errorCallBack?: (errorMessage: string) => void): Promise<{ totalUsersCount: number; users: List_User[] }> {
+    const observable: Observable<{ totalUsersCount: number; users: List_User[] }> = this.httpClientService.get({
+      controller: "users",
+      queryString: `page=${page}&size=${size}`
+    });
+
+    const promiseData = firstValueFrom(observable);
+    promiseData.then(value => successCallBack())
+      .catch(error => errorCallBack(error));
+
+    return await promiseData;
+  }
+
+  async assignRoleToUser(id: string, roles: string[], successCallBack?: () => void, errorCallBack?: (error) => void) {
+    const observable: Observable<any> = this.httpClientService.post({
+      controller: "users",
+      action: "assign-role-to-user"
+    }, {
+      userId: id,
+      roles: roles
+    });
+
+    const promiseData = firstValueFrom(observable);
+    promiseData.then(() => successCallBack())
+      .catch(error => errorCallBack(error));
+
+    await promiseData;
+  }
+
+  async getRolesToUser(userId: string, successCallBack?: () => void, errorCallBack?: (error) => void): Promise<string[]> {
+    const observable: Observable<{ userRoles: string[] }> = this.httpClientService.get({
+      controller: "users",
+      action: "get-roles-to-user"
+    }, userId);
+
+    const promiseData = firstValueFrom(observable);
+    promiseData.then(() => successCallBack())
+      .catch(error => errorCallBack(error));
+
+    return (await promiseData).userRoles;
   }
 }
