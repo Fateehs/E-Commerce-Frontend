c512127b5600a6a7fdf4ab1d44f78091aa346c62
Fateeh <benzer1010@gmail.com>
2023-05-04 09:09:27 +0300

    Authorize Menu Feature modified

--- src/app/dialogs/authorize-menu-dialog/authorize-menu-dialog.component.ts
+++ src/app/dialogs/authorize-menu-dialog/authorize-menu-dialog.component.ts
@@ -1,16 +1,51 @@
 import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSelectionList } from '@angular/material/list';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { SpinnerType } from '../../base/base.component';
+import { List_Role } from '../../contracts/role/List_Role';
+import { RoleService } from '../../services/common/models/role.service';
 import { BaseDialog } from '../base/base-dialog';
+import { AuthorizationEndpointService } from 'src/app/services/common/models/authorization-endpoint.service';
 
 @Component({
   selector: 'app-authorize-menu-dialog',
   templateUrl: './authorize-menu-dialog.component.html',
   styleUrls: ['./authorize-menu-dialog.component.scss']
 })
-export class AuthorizeMenuDialogComponent extends BaseDialog<AuthorizeMenuDialogComponent>  {
+export class AuthorizeMenuDialogComponent extends BaseDialog<AuthorizeMenuDialogComponent> implements OnInit {
   constructor(dialogRef: MatDialogRef<AuthorizeMenuDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private roleService: RoleService,
+    private authorizationEndpointService: AuthorizationEndpointService,
+    private spinner: NgxSpinnerService) {
     super(dialogRef)
+  }
+  roles: { datas: List_Role[], totalCount: number };
+  assignedRoles: Array<string>;
+  listRoles: { name: string, selected: boolean }[];
+  async ngOnInit() {
+    this.assignedRoles = await this.authorizationEndpointService.getRolesToEndpoint(this.data.code, this.data.menuName);
+
+    this.roles = await this.roleService.getRoles(-1, -1);
+
+    this.listRoles = this.roles.datas.map((r: any) => {
+      return {
+        name: r.name,
+        selected: this.assignedRoles?.indexOf(r.name) > -1
+      }
+    });
+  }
+
+  assignRoles(rolesComponent: MatSelectionList) {
+    const roles: string[] = rolesComponent.selectedOptions.selected.map(o => o._text.nativeElement.innerText)
+    this.spinner.show(SpinnerType.BallAtom);
+    this.authorizationEndpointService.assignRoleEndpoint(roles, this.data.code, this.data.menuName,
+      () => {
+        this.spinner.hide(SpinnerType.BallAtom);
+      }, error => {
+
+      })
   }
 }
 
